981266674a73f13f79eb0c0a21f2acf9cad9f716
Augusto Lopes <augustolfp@gmail.com>
2022-09-11 12:26:19 -0300

    feat(Sign-in): Add middleware to validate sign-in request body

--- src/schemas/authSchemas.ts
+++ src/schemas/authSchemas.ts
@@ -1,8 +1,13 @@
 import Joi from "joi";
-import { IUserData } from "../types/userTypes";
+import { IUserData, ILoginUserData } from "../types/userTypes";
 
 export const signUpSchema = Joi.object<IUserData>({
     name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(10).required()
+});
+
+export const signInSchema = Joi.object<ILoginUserData>({
     email: Joi.string().email().required(),
     password: Joi.string().min(10).required()
 });
